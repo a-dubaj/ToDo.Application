50679518fc9986c414adcdc90ec9ba5ecd9700cd
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2018-04-13 00:21:26 +0200

    Implement database driver: 'DatabaseHandler'

--- src/sample/Database/DatabaseHandler.java
+++ src/sample/Database/DatabaseHandler.java
@@ -1,7 +1,22 @@
 package sample.Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by andrzejdubaj on 12.04.2018.
  */
-public class DatabaseHandler {
+public class DatabaseHandler extends Configs{
+    Connection dbConnection;
+
+    public Connection getDbConnection() throws ClassNotFoundException, SQLException {
+        String connectionString = "jdbc:msql://" + dbHost + ":"
+                +dbPort + "/"
+                +dbName;
+
+        Class.forName("com.mysql.jdbc.Driver");
+        dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
+        return dbConnection;
+    }
 }
